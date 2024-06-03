ce7c46dcc60825174d95b0e534b8c4294b17032e
ashiqur2279 <ashiqurrahman2279@gmail.com>
2024-06-03 14:24:16 +0600

    project file launched and header complete

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../assets/task.png";
+
+const Header = () => {
+  return (
+    <div className="flex h-20 py-2 px-4 bg-blue-800 justify-between items-center ">
+      <img className="size-16" src={logo} alt="my task manager" />
+      <h1 className="text-2xl font-semibold text-white">Ashiqur2279</h1>
+    </div>
+  );
+};
+
+export default Header;
